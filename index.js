11a70f7f84bf04ddbdd5ec8795755737027f6d56
Jecidhz18 <jecidhz18@hotmail.com>
2022-08-13 13:24:59 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Por fin node');
+console.log('---------------------')
+console.log(2 + 2);
+console.log('---------------------')
+console.log(5 * 5)
+console.log('---------------------')
